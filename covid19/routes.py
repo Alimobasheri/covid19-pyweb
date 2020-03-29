8a8c8d16ceff381646c34e00e0cf5f06436b591a
Alimobasheri <Alimobasheri@users.noreply.github.com>
2020-03-30 00:10:13 +0430

    Data rows ranges filtering based on page number

--- covid19/routes.py
+++ covid19/routes.py
@@ -1,6 +1,7 @@
 import asyncio
-from flask import render_template
 import json
+
+from flask import redirect, url_for, render_template
 
 from covid19 import app
 from covid19 import csv_parse
@@ -9,23 +10,34 @@ from covid19 import utils
 
 @app.route('/')
 def index():
+	return redirect(url_for('tables', page=1))
+
+@app.route('/tables/<page>')
+def tables(page):
 	with open('covid19/options.json', 'r') as options:
-	  appOptions = json.load(options)
-	  
-	  date = {'day':25, 'month':3, 'year':2020}
-	  fileName = utils.parseDateToFilename(date, appOptions['DATA_FILE_FORMAT'])
-	  
-	  loop = asyncio.new_event_loop()
-	  asyncio.set_event_loop(loop)
-	  data = loop.run_until_complete(fetch_data.getData(fileName, appOptions['COVID_DATA_BASE_URL']))
-	  loop.stop()
-	  dataList = csv_parse.csvToArray(data)
-	  tableHeaders = dataList[0]
-	  tableRows = dataList[1:40]
-	  return render_template(
-	  	'index.html', 
-	  	title='Covid-19 App',
-	  	chart_title='covid19 charts', 
-	  	chart_date=date,
-	  	table_headers=tableHeaders, 
-	  	table_rows=tableRows)
+		appOptions = json.load(options)
+		
+		date = {'day':28, 'month':3, 'year':2020}
+		fileName = utils.parseDateToFilename(date, appOptions['DATA_FILE_FORMAT'])
+		
+		loop = asyncio.new_event_loop()
+		asyncio.set_event_loop(loop)
+		data = loop.run_until_complete(fetch_data.getData(fileName, appOptions['COVID_DATA_BASE_URL']))
+		loop.stop()
+		
+		dataList = csv_parse.csvToArray(data)
+		sortedList = sorted(dataList[1:len(dataList)-1], key=lambda x: x[3])
+		
+		filteredIndices = [3, 4, 7, 8, 9]
+		tableHeaders = [dataList[0][x] for x in filteredIndices]
+		
+		indexZero = (1, (int(page)-1)*50)[int(page) is not None]
+		tableRows = [[x[y] for y in filteredIndices] for x in sortedList[indexZero: indexZero+50]]
+		
+		return render_template(
+		'index.html',
+		title='Covid-19 App',
+		chart_title='covid19 charts',
+		chart_date=date,
+		table_headers=tableHeaders,
+		table_rows=tableRows)
