b82decd6b8a8ba5e92e8ee1fee4425e8b2813788
Alimobasheri <Alimobasheri@users.noreply.github.com>
2020-03-30 15:36:27 +0430

    Updated: chart.js for new design

--- covid19/static/charts.js
+++ covid19/static/charts.js
@@ -1,6 +1,6 @@
 const ChartColumns = columns =>
 	columns.reduce((titles, title, idx) =>
-		`${titles}<td key='${idx}'>
+		`${titles}<td key='${idx}' class='chart-head-td'>
 			${title}
 		</td>`
 	,'')
@@ -9,19 +9,26 @@ const ChartRows = rows =>
 	rows.reduce((accrows, row, ridx) =>
 		accrows+`<tr key=${ridx}>`+
 			row.reduce((items, item, itidx) =>
-				`${items}<td key=${itidx}>${item}</td>`
+				`${items}<td key=${itidx} class='chart-body-td'>${item}</td>`
 			,'')
 		+'</tr>'
 	,'')
 
 const Chart = (title, date, columns, rows) => 
-	`<h1 class='chart-title'>${title}</h1>
-	<span class='chart-date'>${date.day}/${date.month}/${date.year}</span>
-	<table class='chart'>
-		<thead>
-			${ChartColumns(columns)}
-		</thead>
-		<tbody>
-			${ChartRows(rows)}
-		</tbody>
-	</table>`
+	`
+	<div class='chart-wrapper'>
+		<div class='chart-title-date-wrapper'>
+			<h1 class='chart-title'>${title}</h1>
+			<span class='chart-date'>${date.day}/${date.month}/${date.year}</span>
+		</div>
+		<table class='chart-table'>
+			<thead class='chart-head'>
+				<tr class='chart-head-row'>
+					${ChartColumns(columns)}
+				</tr>
+			</thead>
+			<tbody class='chart-body'>
+				${ChartRows(rows)}
+			</tbody>
+		</table>
+	</div>`
