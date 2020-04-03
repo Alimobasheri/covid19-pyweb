859e5504c2f8ae06febb019ef58231543ffc54a0
Mir Ali Mobasheri <Alimobasheri@users.noreply.github.com>
2020-04-03 20:04:15 +0430

    Added: new datetime functionalities

--- covid19/utils.py
+++ covid19/utils.py
@@ -1,2 +1,10 @@
 def parseDateToFilename(date, fExtension):
-	return(f"{date['month']:02d}-{date['day']:02d}-{date['year']:04d}.{fExtension}")
+	return(f"{date.strftime('%m-%d-%Y')}.{fExtension}")
+
+from datetime import datetime, timedelta
+
+def getDiffDate(today, days):
+	return today + timedelta(days=days)
+
+def getLatestDate(today, diff):
+	return getDiffDate(today, diff).strftime('%m-%d-%Y')
